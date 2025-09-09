2651129ab85913ed55ecf7517fa96ce0c369ff54
Luke Morcom <luke@morcom.io>
2025-09-09 12:51:41 +0100

    goroutines, bundle mqtt-web and expose websocket listener

--- publisher/service/service.go
+++ publisher/service/service.go
@@ -5,6 +5,7 @@ import (
 	"github.com/lukemorcom/bodmqtt/config"
 	"github.com/lukemorcom/bodmqtt/publisher"
 	"github.com/lukemorcom/bodmqtt/strategies"
+	"sync"
 )
 
 type Service struct {
@@ -16,32 +17,41 @@ func NewService(cfg *config.Config) *Service {
 }
 
 func (s *Service) Run() {
+	var wg sync.WaitGroup
+
+	pub := publisher.NewMQTTPublisher("localhost:1883")
+
 	for _, event := range s.cfg.Events {
-		var strat strategies.Strategy
-
-		switch event.API.Strategy {
-		case config.StrategyPing:
-			strat = strategies.PingStrategy{}
-		default:
-			fmt.Printf("Unexpected strategy name: %s\n", event.API.Strategy)
-			continue
-		}
-
-		status, err := strat.Execute(event)
-
-		if err != nil {
-			fmt.Printf("Error executing event %s: %v\n", event.Name, err)
-			continue
-		}
-
-		pub := publisher.NewMQTTPublisher("localhost:1883")
-
-		err = pub.Publish(status, event.Topic)
-
-		if err != nil {
-			fmt.Printf("Error publishing event %s on topic %s: %v\n", event.Name, event.Topic, err)
-		}
-
-		continue
+		wg.Add(1)
+		go workEvent(event, pub, &wg)
 	}
+
+	wg.Wait()
+}
+
+func workEvent(event config.Event, pub *publisher.MQTTPublisher, wg *sync.WaitGroup) {
+	var strat strategies.Strategy
+
+	switch event.API.Strategy {
+	case config.StrategyPing:
+		strat = strategies.PingStrategy{}
+	default:
+		fmt.Printf("Unexpected strategy name: %s\n", event.API.Strategy)
+		return
+	}
+
+	status, err := strat.Execute(event)
+
+	if err != nil {
+		fmt.Printf("Error executing event %s: %v\n", event.Name, err)
+		return
+	}
+
+	err = pub.Publish(status, event.Topic)
+
+	if err != nil {
+		fmt.Printf("Error publishing event %s on topic %s: %v\n", event.Name, event.Topic, err)
+	}
+
+	wg.Done()
 }
